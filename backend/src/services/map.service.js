7c37de94634bdf53e5ba3375d287d7f870a37912
abdulhaque2005 <abdulhaquecg@gmail.com>
2026-05-26 19:27:10 +0530

    Update server and services (Batch 4)

---
+++ backend/src/services/map.service.js
@@ -0,0 +1,36 @@
+import Earthquake from '../models/Earthquake.js';
+
+class MapService {
+
+  async getEarthquakesWithinRadius(latitude, longitude, radiusKm = 500, limit = 100) {
+
+    const radiusInRadians = radiusKm / 6378.1;
+
+    return await Earthquake.find({
+      location: {
+        $geoWithin: {
+          $centerSphere: [[longitude, latitude], radiusInRadians],
+        },
+      },
+    })
+      .sort('-time')
+      .limit(limit);
+  }
+
+  async getEarthquakesInBoundingBox(swLng, swLat, neLng, neLat, limit = 200) {
+    return await Earthquake.find({
+      location: {
+        $geoWithin: {
+          $box: [
+            [swLng, swLat],
+            [neLng, neLat],
+          ],
+        },
+      },
+    })
+      .sort('-time')
+      .limit(limit);
+  }
+}
+
+export default new MapService();
